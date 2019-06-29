cde90318f566e73943fddc91a823981474d3bd4e
Alex Luong <aluong@crimson.ua.edu>
2019-06-29 15:54:38 -0400

    Init

---
+++ src/lambda/hello.js
@@ -0,0 +1,10 @@
+export function handler(event, context, callback) {
+  console.log("queryStringParameters", event.queryStringParameters)
+
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      msg: "Hello, World! " + Math.round(Math.random() * 10),
+    }),
+  })
+}
